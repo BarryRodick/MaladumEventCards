3f1683964e82dde7642d230b872c9b3f443db97c
Barry <Barry@Assisted-Review.com>
2024-10-08 20:04:54 +0100

    updated version

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js for PWA functionality
 
-const CACHE_NAME = 'deck-builder-v1.03'; // Incremented version
+const CACHE_NAME = 'deck-builder-v1.05'; // Incremented version
 const urlsToCache = [
     './',
     './index.html',
