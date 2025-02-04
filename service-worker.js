cc2a4f4cad858ef92bf78ee193980b4132ee5821
BarryRodick <123958824+BarryRodick@users.noreply.github.com>
2025-02-04 09:57:36 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js for PWA functionality
 
-const CACHE_NAME = 'deck-builder-v1.7'; // Increment version
-const APP_VERSION = '1.7'; // Add version number
+const CACHE_NAME = 'deck-builder-v1.8'; // Incremented version
+const APP_VERSION = '1.8'; // Incremented version
 const urlsToCache = [
     './',
     './index.html',
